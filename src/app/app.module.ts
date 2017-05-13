4e9e1fa43172bb32cc0d0fef30f6153fe77b12b9
Kierancz <kieran.czerwinski@colorado.edu>
2017-05-12 23:36:06 -0600

    added youtube api search service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,27 @@
-import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
-import { AppComponent }  from './app.component';
+import { AppComponent } from './app.component';
+import { YouTubeSearchComponent } from './youtube-search/youtube-search.component';
+import { SearchResultComponent } from './youtube-search/search-result.component';
+import { SearchBoxComponent } from './youtube-search/search-box.component';
+import { youTubeSearchInjectables } from './youtube-search/youtube-search.injectables';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    YouTubeSearchComponent,
+    SearchResultComponent,
+    SearchBoxComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule 
+  ],
+  providers: [youTubeSearchInjectables],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
