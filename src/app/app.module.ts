b862f2fe50dc017e10f4f64f28985c57a51e1a0c
Kierancz <kieran.czerwinski@colorado.edu>
2017-05-14 23:42:40 -0600

    setting up basic routes and auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,64 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { YouTubeSearchComponent } from './youtube-search/youtube-search.component';
 import { SearchResultComponent } from './youtube-search/search-result.component';
 import { SearchBoxComponent } from './youtube-search/search-box.component';
 import { youTubeSearchInjectables } from './youtube-search/youtube-search.injectables';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProtectedComponent } from './protected/protected.component';
+
+import { AUTH_PROVIDERS } from './auth.service';
+import { LoggedInGuard } from './logged-in.guard';
+
+
+const routes: Routes = [
+  // basic routes
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+
+  // authentication
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'protected',
+    component: ProtectedComponent,
+    canActivate: [ LoggedInGuard ]
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     YouTubeSearchComponent,
     SearchResultComponent,
-    SearchBoxComponent
+    SearchBoxComponent,
+    AboutComponent,
+    HomeComponent,
+    LoginComponent,
+    ProtectedComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes) // import & install routes
   ],
-  providers: [youTubeSearchInjectables],
+  providers: [
+    youTubeSearchInjectables,
+    // uncomment this for "hash-bang" routing
+    // { provide: LocationStrategy, useClass: HashLocationStrategy }
+    AUTH_PROVIDERS,
+    LoggedInGuard
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+
+}
